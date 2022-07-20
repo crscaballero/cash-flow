fdd737adad99e5cbb68238d94110b05f57fc9c5b
CrsCaballero <crsecaballero@gmail.com>
2022-07-19 20:28:35 -0700

    Init project with some progress

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+	<header>
+		<img
+			src="@/assets/cashflow-icon.svg"
+			class=""
+			alt="Logo"
+			@click="reload"
+		/>
+		<h1><span>Cash</span>Flow</h1>
+		<span></span>
+	</header>
+</template>
+<script>
+export default {
+	methods: {
+		reload() {
+			location.reload();
+		},
+	},
+};
+</script>
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	width: 100vw;
+}
+img,
+span {
+	width: 48px;
+}
+h1 {
+	color: var(--brand-blue);
+}
+h1 span {
+	color: var(--brand-green);
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,104 @@
+<template>
+	<Layout>
+		<template #header>
+			<Header />
+		</template>
+		<template #resume>
+			<Resume
+				:totalLabel="'Total savings'"
+				:label="label"
+				:totalAmount="100000"
+				:amount="amount"
+			>
+				<template #graphic> Graphic </template>
+				<template #action> Action </template>
+			</Resume>
+		</template>
+		<template #movements>
+			<Movements :movements="movements" />
+		</template>
+	</Layout>
+</template>
+<script>
+import Header from '@/components/Header.vue';
+import Layout from '@/components/Layout.vue';
+import Resume from '@/components/Resume/Index.vue';
+import Movements from '@/components/Movements/Index.vue';
+
+export default {
+	components: {
+		Header,
+		Layout,
+		Resume,
+		Movements,
+	},
+	data() {
+		return {
+			label: null,
+			amount: null,
+			movements: [
+				{
+					id: 0,
+					title: 'Movement',
+					description: 'Salary deposit',
+					amount: 1000,
+				},
+				{
+					id: 1,
+					title: 'Movement 1',
+					description: 'Deposit of fees',
+					amount: 500,
+				},
+				{
+					id: 2,
+					title: 'Movement 2',
+					description: 'Food',
+					amount: -100,
+				},
+				{
+					id: 3,
+					title: 'Movement 3',
+					description: 'Tuition',
+					amount: 1000,
+				},
+				{
+					id: 4,
+					title: 'Movement 4',
+					description: 'Computer repair',
+					amount: 1000,
+				},
+				{
+					id: 5,
+					title: 'Movement 5',
+					description: 'Computer repair',
+					amount: 1000,
+				},
+				{
+					id: 6,
+					title: 'Movement 6',
+					description: 'Computer repair',
+					amount: 1000,
+				},
+				{
+					id: 7,
+					title: 'Movement 7',
+					description: 'Computer repair',
+					amount: 1000,
+				},
+				{
+					id: 8,
+					title: 'Movement 8',
+					description: 'Computer repair',
+					amount: 1000,
+				},
+				{
+					id: 9,
+					title: 'Movement 9',
+					description: 'Computer repair',
+					amount: 1000,
+				},
+			],
+		};
+	},
+};
+</script>
